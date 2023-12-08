86c3a141b1ea8bdf9a5f948027918f795da227bc
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-12-08 18:27:54 +0200

    queue

---
+++ QueueTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            Console.WriteLine(q.empty());
+            Console.WriteLine(q.full());
+
+            q.enqueue(5); //5
+            q.enqueue(9); //5,9
+            Console.WriteLine(q.empty());
+
+            Console.WriteLine(q.dequeue()); //9 in queue
+
+            q.enqueue(13); // 9,13
+            q.enqueue(10); // 9,13,10
+
+            int x = q.dequeue();
+            Console.WriteLine(x); // 13,10
+
+             x = q.dequeue();
+            Console.WriteLine(x); //10
+
+            x = q.dequeue();
+            Console.WriteLine(x); //0
+
+            x = q.dequeue();
+            Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ QueueTest/Queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTest
+{
+    internal class Queue
+    {
+        private int first = -1;
+        private int last = -1;
+        private int[] arr = new int[10];
+
+        public Boolean empty()
+        {
+            return first == last;
+
+        }
+
+        public Boolean full()
+        {
+            return last == arr.Length - 1;
+        }
+
+        public void enqueue (int v)
+        {
+            if (this.full())
+            {
+                Console.WriteLine("Queue is full!!");
+            }
+            else
+            {
+                last++;
+                arr[last] = v;
+            }
+            
+        }
+
+        public int dequeue()
+        {
+            if (this.empty())
+            {
+                Console.WriteLine("Queue is empty!!");
+                return -1;
+            }
+            else
+            {
+                first++;
+                return arr[first];
+            }
+            
+        }
+
+    }
+}
