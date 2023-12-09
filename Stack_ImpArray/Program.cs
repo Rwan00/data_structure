5998178425dcc7cff31685f48371e10b9f2c2a9a
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-12-09 20:32:46 +0200

    add Stack

---
+++ Stack_ImpArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_ImpArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stackk MyStack = new Stackk();
+
+            MyStack.push(5);
+            MyStack.push(9);
+            MyStack.push(3);
+
+            MyStack.pop();
+
+
+            MyStack.print();
+
+
+          
+
+
+        }
+    }
+}
---
+++ Stack_ImpArray/Stackk.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_ImpArray
+{
+    public class Stackk
+    {
+
+        private int[] arr = new int[3];
+        private int TopIndex = -1;
+
+        public void push(int s)
+        {
+            if (TopIndex == arr.Length - 1)
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+
+            }
+            else
+            {
+                TopIndex++;
+                arr[TopIndex] = s;
+            }
+
+
+        }
+        public int pop()
+        {
+
+            if (TopIndex == -1)
+            {
+                Console.WriteLine("Stack is empty!!");
+                return -1;
+            }
+            else
+            {
+                int v = arr[TopIndex];
+
+                v = arr[TopIndex--];
+                Console.WriteLine("Element poped: " + v);
+
+                return v;
+            }
+
+        }
+        public void print()
+        {
+            if (TopIndex == -1)
+            {
+                Console.WriteLine("Stack is empty!!");
+
+            }
+            else
+            {
+                for (int i = 0; i <= TopIndex; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+
+
+      
+    }
+}
+
+
+
+
+
+
+
+
+                    
